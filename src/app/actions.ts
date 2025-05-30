7bacaef5e365e62c73455a8dae786c2e23ab7110
xd-Abi <abinayansureskumar@proton.me>
2025-05-31 01:33:31 +0200

    Updated form to create teams

---
+++ src/app/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { prisma } from "@/app/prisma";
+import { getSession } from "@/utils/jwt";
+import { nanoid } from "nanoid";
+import { redirect } from "next/navigation";
+
+export async function createTeam(formData: FormData) {
+  const session = await getSession();
+  if (!session) redirect("/login");
+
+  const team = await prisma.team.create({
+    data: {
+      id: nanoid(),
+      slug: formData.get("slug")?.toString() ?? "",
+      name: formData.get("slug")?.toString() ?? "",
+      description: formData.get("description")?.toString() || undefined,
+      website: formData.get("website")?.toString() || undefined,
+      location: formData.get("location")?.toString() || undefined,
+      memberships: {
+        create: {
+          uid: session.user.id,
+          role: "Owner",
+        },
+      },
+    },
+  });
+
+  redirect(`/${team.slug}`);
+}
