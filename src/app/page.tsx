2b49c1069ae1e64f72f7db14a40ffd3e83909b97
xd-Abi <abinayansureskumar@proton.me>
2025-05-30 17:00:45 +0200

    Redirecting the user to login automatically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,9 @@ import { auth, signIn } from "@/app/auth";
 export default async function Home() {
   const session = await auth();
 
-  if (!session?.user) return null;
+  if (!session?.user) {
+    await signIn("github", { redirectTo: "/" });
+  }
 
-  return (
-    <main>
-      <form
-        action={async () => {
-          "use server";
-          await signIn("github", { redirectTo: "/" });
-        }}
-      >
-        <button type="submit">Sign in</button>
-      </form>
-      {JSON.stringify(session)}
-    </main>
-  );
+  return <main>{JSON.stringify(session)}</main>;
 }
