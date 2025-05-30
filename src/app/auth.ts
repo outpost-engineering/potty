891515db6f65a18f2d881dcd07bca3379dae3909
xd-Abi <abinayansureskumar@proton.me>
2025-05-30 16:00:47 +0200

    Updated next-auth version to fix errors

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import Github from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/app/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub({
+    Github({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
     }),
