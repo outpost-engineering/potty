f811cfe865d9304f018cb640e15f6a0a0b9d9760
Michelstuder <107350826+Michelstuder@users.noreply.github.com>
2025-06-04 20:30:00 +0200

    Added link to google redirect uri

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -44,7 +44,7 @@ export default async function LoginPage(props: Props) {
           <LoginButton
             provider="Google"
             icon={<GoogleIcon className="size-6" />}
-            href=""
+            href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.AUTH_GOOGLE_ID}&response_type=code&scope=${encodeURIComponent("openid email profile")}&redirect_uri=${encodeURIComponent(`${process.env.BASE_URL}/api/auth/google`)}`}
           />
           <LoginButton
             provider="Microsoft"
