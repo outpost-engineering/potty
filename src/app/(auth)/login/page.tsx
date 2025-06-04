9879f9ba9d7147a9a448bf4338b8effedec184ff
xd-Abi <abinayansureskumar@proton.me>
2025-06-04 13:30:31 +0200

    Added simple link to allow github logins

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,30 @@
-export default function Login() {
-  return <main className="container mx-auto bg-purple-700">Login</main>;
+import { redirect } from "next/navigation";
+import { getSession } from "~/utils/session";
+
+export default async function Login() {
+  const session = await getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <main className="container mx-auto bg-purple-700">
+      Login
+      <br />
+      <a
+        href={
+          "https://github.com/login/oauth/authorize" +
+          "?client_id=" +
+          process.env.AUTH_GITHUB_ID +
+          "&scope=" +
+          encodeURIComponent("read:user user:email") +
+          "&redirect_uri=" +
+          encodeURIComponent(`${process.env.BASE_URL}/api/auth/github`)
+        }
+      >
+        Login with GitHub
+      </a>
+    </main>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import { getSession } from "~/utils/session";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if (session) {
+    return <main>Logged in</main>;
+  }
+
   return <main>Home</main>;
 }
