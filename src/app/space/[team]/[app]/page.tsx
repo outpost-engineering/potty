ac625d5d93414b07816f270cb59c93ffed60fa6b
Michelstuder <107350826+Michelstuder@users.noreply.github.com>
2025-06-10 22:39:33 +0200

    Creating api key on page render

--- src/app/space/[team]/[app]/page.tsx
+++ src/app/space/[team]/[app]/page.tsx
@@ -1,3 +1,7 @@
+import { notFound } from "next/navigation";
+import { prisma } from "~/libs/prisma";
+import { createApiKey } from "./actions";
+
 interface Params {
   team: string;
   app: string;
@@ -9,6 +13,33 @@ interface Props {
 
 export default async function App(props: Props) {
   const params = await props.params;
+  const team = await prisma.team.findUnique({
+    where: { slug: params.team },
+    include: { apps: true },
+  });
 
-  return <main>App: {JSON.stringify(params)}</main>;
+  if (!team) {
+    return notFound();
+  }
+
+  const app = await prisma.app.findUnique({
+    where: {
+      tid_name: {
+        name: params.app,
+        tid: team.id,
+      },
+    },
+  });
+
+  if (!app) {
+    return notFound();
+  }
+
+  await createApiKey(team, app.id);
+
+  return (
+    <main>
+      <h1>App</h1>
+    </main>
+  );
 }
