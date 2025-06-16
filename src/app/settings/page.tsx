e83efc772494e9e0523703eb6bfd9a0a66dc381b
Matteo Gisler <matteo.gisler05@gmail.com>
2025-06-16 22:36:55 +0200

    Implemented metadata in settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,10 @@
+import { Metadata } from "next";
 import { SettingsContent } from "~/components/settings/settings-content";
 import { auth } from "~/libs/auth";
 
-// export const metadata: Metadata = {
-//   title: "General - Account Settings",
-// };
+export const metadata: Metadata = {
+  title: "General - Account Settings",
+};
 
 export default async function SettingsPage() {
   const session = await auth();
