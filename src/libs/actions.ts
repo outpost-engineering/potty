486792024a1272e3cf170365a48044aead65943f
Michelstuder <107350826+Michelstuder@users.noreply.github.com>
2025-06-20 00:50:22 +0200

    Creating pendingKitchen before checkout process

--- src/libs/actions.ts
+++ src/libs/actions.ts
@@ -3,24 +3,11 @@
 import { z } from "zod";
 import { auth, signOut } from "./auth";
 import { prisma } from "./prisma";
+import { stripe } from "./stripe";
 
 const impressionSchema = z.object({
   note: z.string().min(1).max(1500),
   emotion: z.enum(["😍", "😁", "😟", "😭"]).optional(),
-});
-
-const createKitchenSchema = z.object({
-  name: z.string().min(1).max(50),
-  slug: z
-    .string()
-    .min(1)
-    .max(50)
-    .regex(/^[a-z0-9-]+$/),
-  description: z.string().max(255).optional(),
-  image: z.string().url().optional(),
-  website: z.string().url().optional(),
-  location: z.string().max(100).optional(),
-  email: z.string().email(),
 });
 
 export async function logout() {
@@ -96,41 +83,47 @@ export async function startKitchenCheckout(
     const session = await auth();
     if (!session) return false;
 
+    const userId = session.user?.id;
     const email = session.user?.email;
-    const result = createKitchenSchema.safeParse({
-      name,
-      slug,
-      email,
-      description,
-      image,
-      website,
-      location,
-    });
-    if (!result.success) return false;
+    if (!userId || !email) return false;
 
-    const res = await fetch(
-      `${process.env.BASE_URL}/api/stripe/create-checkout-session`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          name,
-          slug,
-          email,
-          description,
-          image,
-          website,
-          location,
-        }),
+    const pending = await prisma.pendingKitchen.create({
+      data: {
+        userId,
+        name,
+        slug,
+        description: description || "",
+        image: image || "",
+        website: website || "",
+        location: location || "",
+        stripeSessionId: "asdf",
       },
-    );
+    });
 
-    const data = await res.json();
+    const stripeSession = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      customer_email: email,
+      line_items: [
+        {
+          price: process.env.STRIPE_BASE_PLAN_PRICE_ID!,
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.BASE_URL}/${slug}`,
+      cancel_url: `${process.env.BASE_URL}/kitchen/create`,
+      metadata: {
+        pendingKitchenId: pending.id,
+      },
+    });
 
-    console.log("response", data);
+    // Needed for db querying in webhook to avoid duplicate kitchens etc.
+    await prisma.pendingKitchen.update({
+      where: { id: pending.id },
+      data: { stripeSessionId: stripeSession.id },
+    });
 
-    if (!res.ok || !data.url) return false;
-
-    return data.url;
+    return stripeSession.url;
   } catch {
     return false;
   }
